e4a417f8a5b7dd5731f14639075eaf406360fa01
CAIO VINICIUS DA SILVA <caiospfc88@gmail.com>
2024-11-03 21:37:31 -0300

    construção dos items do submenu

--- components/primeira-moda/ListaSubmenus.tsx
+++ components/primeira-moda/ListaSubmenus.tsx
@@ -1,19 +1,19 @@
 "use client"
 
 import { opcoesSubmenu } from '@/Types/submenu'
-import { Box, HStack, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { Box, HStack, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { FaChevronRight } from 'react-icons/fa6'
 import { RxDotFilled } from 'react-icons/rx'
 
 const ListaSubmenus = () => {
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState<number | null>(null);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState<number | null>(null); 
 
     return (
         <Box w={"100vw"} h={"2vw"} bgColor="#F8F6F2" display={"flex"} justifyContent={"center"}>
             <HStack as="ul" spacing={8}>
                 {opcoesSubmenu.map((itemMenu) => (
-                    <Box key={itemMenu.id.toString()}>
+                    <Box key={itemMenu.id.toString()} position="relative">
                         <Menu>
                             <MenuButton as={Box} bgColor={"transparent"} cursor={"pointer"}>
                                 <Box display={"flex"} flexDir={"row"} color={"#7DE3B0"} gap={"5px"} fontSize={"1.1rem"} fontWeight={"500"}>
@@ -25,48 +25,47 @@ const ListaSubmenus = () => {
                             </MenuButton>
 
                             <MenuList p={0}>
-                                <Box display={"flex"} position="relative">
-                                    <Box>
-                                        {itemMenu.opcoes.map((subItem) => (
-                                            <MenuItem
-                                                minH="48px"
-                                                key={subItem.id}
-                                                as={Box}
-                                                cursor={"pointer"}
-                                                onMouseEnter={() => setIsSubmenuOpen(subItem.id)}
-                                                onMouseLeave={() => setIsSubmenuOpen(null)}
-                                            >
-                                                <Box display={"flex"} flexDir={"row"} color={"#69593C"}>
-                                                    {subItem.label}
-                                                    <Box fontSize={"10px"} display={"flex"} justifyContent={"center"} flexDir={"column"} ml={"8px"}>
-                                                        <FaChevronRight />
-                                                    </Box>
+                                <Box display={"flex"} flexDir={"column"}>
+                                    {itemMenu.opcoes.map((subItem) => (
+                                        <MenuItem
+                                            minH="48px"
+                                            key={subItem.id}
+                                            as={Box}
+                                            cursor={"pointer"}
+                                            position="relative"
+                                            onMouseEnter={() => setIsSubmenuOpen(subItem.id)}
+                                            onMouseLeave={() => setIsSubmenuOpen(null)}
+                                        >
+                                            <Box display={"flex"} flexDir={"row"} color={"#69593C"}>
+                                                {subItem.label}
+                                                <Box fontSize={"10px"} display={"flex"} justifyContent={"center"} flexDir={"column"} ml={"8px"}>
+                                                    <FaChevronRight />
                                                 </Box>
-
-                                                {/* Submenu */}
-                                                {isSubmenuOpen === subItem.id && (
-                                                    <Box w={"10vw"}
+                                            </Box>
+                                            <Box w={"10vw"} position={"relative"}>
+                                            {/* Submenu */}
+                                            {isSubmenuOpen === subItem.id && (
+                                                <Box
+                                                    position="absolute"
+                                                    left="10%" // Posiciona o submenu à direita do item principal
+                                                    top="0"
                                                     bg="white"
+                                                    zIndex="dropdown"
+                                                    minWidth="150px"
+                                                    p="4"
                                                     onMouseEnter={() => setIsSubmenuOpen(subItem.id)}
                                                     onMouseLeave={() => setIsSubmenuOpen(null)}
-                                                    >
-                                                        {subItem.opcoesfinais.map((opcaoFinal) => (
-                                                            <MenuItem key={opcaoFinal.id}>
-                                                                {opcaoFinal.label}
-                                                            </MenuItem>
-                                                        ))}
-                                                    </Box>
-                                                )}
-                                            </MenuItem>
-                                        ))}
-                                    </Box>
-                                    <Box
-                                        w={"9vw"}
-                                        backgroundImage={`url(${itemMenu.imagem})`}
-                                        backgroundSize="cover"
-                                        backgroundPosition="center"
-                                        backgroundRepeat="no-repeat"
-                                    ></Box>
+                                                >
+                                                    {subItem.opcoesfinais.map((opcaoFinal) => (
+                                                        <Text key={opcaoFinal.id} p={2} _hover={{ bg: "gray.100" }}>
+                                                            {opcaoFinal.label}
+                                                        </Text>
+                                                    ))}
+                                                </Box>
+                                            )}
+                                            </Box>
+                                        </MenuItem>
+                                    ))}
                                 </Box>
                             </MenuList>
                         </Menu>
